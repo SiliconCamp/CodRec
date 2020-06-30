e3d6408e8a50b336a5867f9b40f3152ff330a853
SiliconCamp <43041068+SiliconCamp@users.noreply.github.com>
2020-06-30 15:00:13 +0300

    convertor works
    CSV convertor is working now

--- Additional/convert_me.py
+++ Additional/convert_me.py
@@ -3,12 +3,12 @@ from sys import argv
 
 source_file = open(argv[1], 'r')
 result_file = open(argv[2], 'w')
-csv_reader = csv.reader(source_file)
+csv_reader = csv.DictReader(source_file)
 csv_writer = csv.writer(result_file)
 
 for each_line in csv_reader:
-    print(each_line)
-    csv_writer.writerow([each_line])
+    print(each_line["email"], each_line["name"])
+    csv_writer.writerow([each_line["email"], each_line["name"]])
 
 source_file.close()
 result_file.close()
